365c49360cf0080a3d3e35b13714ed87188dd9ed
alberto.inuggi@iit.it <alberto.inuggi@iit.it>
2020-06-03 14:05:54 +0200

    correctly compliling project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,30 @@
-plugins {
-    id(Plugins.androidLibrary)
-    kotlin(Plugins.kotlinAndroid)
-    kotlin(Plugins.kotlinExtensions)
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-    id("name.remal.check-dependency-updates") version "1.0.192"
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        maven {
+            url = uri("https://dl.bintray.com/kyonifer/maven")
+        }
+
+    }
+    // NOTE: Do not place your application dependencies here; they belong in the individual module build.gradle files
+    dependencies {
+        classpath(ClassPaths.gradlePlugin)
+        classpath(ClassPaths.kotlinPlugin)
+        classpath(ClassPaths.navSafeArgsGradlePlugin)
+    }
 }
 
-android {
-
-    compileSdkVersion(Configs.compileSdkVersion)
-    defaultConfig {
-        minSdkVersion(Configs.minSdkVersion)
-        targetSdkVersion(Configs.targetSdkVersion)
-        versionCode = Configs.versionCode
-        versionName = Configs.versionName
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
     }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile(ProGuards.proguardTxt), ProGuards.androidDefault)        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
 }
 
-androidExtensions {
-    isExperimental = true
-}
-
-
-dependencies {
-
-    api(Dependencies.AndroidX.ktxCore)
-    api(Dependencies.AndroidX.navFragment)
-    api(Dependencies.AndroidX.navUi)
-    api(Dependencies.Kotlin.stdLib)
-
-    api("androidx.appcompat:appcompat:1.3.0-alpha01")
-    api("androidx.constraintlayout:constraintlayout:1.1.3")
-    api("com.google.android.material:material:1.1.0")
-
-    api("io.reactivex.rxjava2:rxandroid:2.1.1")
-    api("com.jakewharton.rxrelay2:rxrelay:2.1.1")
-    api("io.reactivex.rxjava2:rxkotlin:2.4.0")
-
-    testImplementation("junit:junit:4.13")
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+tasks.register("clean", Delete::class){
+    delete(rootProject.buildDir)
 }
